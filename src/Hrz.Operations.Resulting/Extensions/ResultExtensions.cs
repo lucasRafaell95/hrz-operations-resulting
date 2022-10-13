fc239413b51e6148214e9fce553cacc21fd1d171
Lucas Rafaell <lucas.rafaell@outloo.com>
2022-10-13 17:40:41 -0300

    feat(result) - implementation of the result contract
    - creation of the error class;
    - creation of the result contract;

---
+++ src/Hrz.Operations.Resulting/Extensions/ResultExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Event.Horizon.Operations.Resulting.Extensions;
+
+/// <summary>
+/// Operation result extensions methods
+/// </summary>
+public static class ResultExtensions
+{
+    /// <summary>
+    /// Extension method for creating result containing error
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="result">Result that will be extended</param>
+    /// <param name="code">Error code</param>
+    /// <param name="message">Error message</param>
+    /// <param name="type">Error type</param>
+    /// <returns></returns>
+    public static Result<TResult> WithError<TResult>(this Result<TResult> result, string code, string message, ErrorType type = ErrorType.BusinessError)
+    {
+        result = new Error(code, message, type);
+
+        return result;
+    }
+}
